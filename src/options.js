a6582884ed46f43063eab0a3f15a002a5772d65a
Nikita Savchenko <zitros.lab@gmail.com>
2025-04-18 21:07:15 +0200

    Update manifest permissions + small style tweaks

--- src/options.js
+++ src/options.js
@@ -8,6 +8,7 @@ import {
 } from "./utils/index.js";
 
 window.addEventListener("load", async () => {
+  const version = document.getElementById("version");
   const delayElement = document.getElementById("delay");
   const maxTabsElement = document.getElementById("maxTabs");
   const movePinnedTabsElement = document.getElementById("movePinnedTabs");
@@ -15,6 +16,7 @@ window.addEventListener("load", async () => {
   delayElement.value = await getDelay();
   maxTabsElement.value = await getMaxTabs();
   movePinnedTabsElement.checked = await getMovePinnedTabs();
+  version.textContent = "v/* @echo pkg.version */";
 
   delayElement.addEventListener("change", async ({ target }) => {
     target.value = await setDelay(target.value);
